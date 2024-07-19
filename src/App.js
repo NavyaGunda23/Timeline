9b9feca3e0fe2197eba585a647713b3144b085c0
NavyaGunda23 <navyagunda.71@gmail.com>
2024-07-19 10:18:25 +0400

    adding react chrono timeline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Chrono } from "react-chrono";
 
-function App() {
+
+const App = () => {
+  const items = [
+    {
+      title: "January 2020",
+      cardTitle: "Card 1",
+      cardSubtitle: "Subtitle 1",
+      cardDetailedText: "Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2"
+    },
+    {
+      title: "February 2020",
+      cardTitle: "Card 2",
+      cardSubtitle: "Subtitle 2",
+      cardDetailedText: "Detailed text 2Detailed text 2Detailed text 2Detailed text 2Detailed text 2"
+    },
+    {
+      title: "March 2020",
+      cardTitle: "Card 3",
+      cardSubtitle: "Subtitle 3",
+      cardDetailedText: "Detailed text 3"
+    }
+  ];
+
+  const [currentItemIndex, setCurrentItemIndex] = useState(0);
+
+  // useEffect(() => {
+  //   console.log("Setting up interval for slideshow");
+  //   const intervalId = setInterval(() => {
+  //     console.log("Changing active item index");
+  //     setCurrentItemIndex((prevIndex) => (prevIndex + 1) % items.length);
+  //   }, 4500); // 4.5 seconds
+
+  //   return () => {
+  //     console.log("Clearing interval");
+  //     clearInterval(intervalId); // Cleanup interval on component unmount
+  //   };
+  // }, [items.length]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100%", height: "500px" }}>
+     <Chrono
+          items={items}
+          mode="HORIZONTAL"
+        
+          cardWidth="100%"
+          cardHeight={300}
+          contentDetailsHeight={100}
+          fontSizes={{
+            title: "1rem"
+          }}
+          slideShow
+          hideControls={true}
+        />
     </div>
   );
-}
+};
 
 export default App;
